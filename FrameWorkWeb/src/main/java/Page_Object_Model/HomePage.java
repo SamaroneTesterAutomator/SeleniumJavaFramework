9b56f8e3deb5fb81e6c17f2a399afd97cf744e7e
= <=>
2022-01-19 21:26:04 -0300

    Inspecionado botão Sign In

--- FrameWorkWeb/src/main/java/Page_Object_Model/HomePage.java
+++ FrameWorkWeb/src/main/java/Page_Object_Model/HomePage.java
@@ -1,12 +1,19 @@
 package Page_Object_Model;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
 
 	//PAGE URL
 	private static String PAGE_URL = "http://automationpractice.com/index.php";
 	private WebDriver driver;
+	
+	//Locators
+	//SignIn Button
+	@FindBy(xpath =  "//a[@class='login']")
+	private WebElement signInButton;
 	
 	
 	
