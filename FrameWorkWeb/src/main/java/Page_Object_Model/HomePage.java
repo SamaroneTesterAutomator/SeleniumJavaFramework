9b7923e43ac9c80160ca27355bf52c8f4612371e
= <=>
2022-01-19 21:17:17 -0300

    Criado Classes HomePage, SignInPage, SignInPageTest.
    Adicionado variáveis: page_url e driver

---
+++ FrameWorkWeb/src/main/java/Page_Object_Model/HomePage.java
@@ -0,0 +1,13 @@
+package Page_Object_Model;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+	//PAGE URL
+	private static String PAGE_URL = "http://automationpractice.com/index.php";
+	private WebDriver driver;
+	
+	
+	
+}
